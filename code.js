502f94de9d30413f748381c11ba47fe0fa8c131b
SergeyTss <64598233+SergeyTss@users.noreply.github.com>
2020-04-30 17:34:17 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,93 @@
+
+//DOM elements
+
+const resultEl = document.getElementById("result")
+const lengthEl = document.getElementById("length")
+const uppercaseEl = document.getElementById("uppercase")
+const lowecaseEl = document.getElementById("lowercase")
+const numberEl = document.getElementById("numbers")
+const symbolsEl = document.getElementById("symbols")
+const generateEl = document.getElementById("generate")
+const clipboardEl = document.getElementById("clipboard")
+
+
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    symbol: getRandomSymbol,
+    number: getRandomNumber
+
+}
+// generate event listener
+generateEl.addEventListener('click', () =>{
+    const length = +lengthEl.value
+    const hasUpper = uppercaseEl.checked
+    const hasLower = lowecaseEl.checked
+    const hasSymbol = symbolsEl.checked
+    const hasNumber = numberEl.checked
+
+resultEl.innerText = generatePassword(hasUpper, hasLower, hasSymbol, hasNumber, length)
+
+})    
+clipboardEl.addEventListener("click", () =>{
+    const textArea = document.createElement("textArea")
+    const password = resultEl.innerText
+
+
+    if(!password){
+        return
+    }
+
+    textArea.value = password
+    document.body.appendChild(textArea)
+    textArea.select()
+    document.execCommand("copy")
+    textArea.remove()
+    alert("Пароль был скопирован!")
+})
+//Generate password function
+
+function generatePassword(upper,lower,symbol,number,length){
+
+
+    let generatedPassword =""
+    const typesCount = upper+lower+symbol+number
+    const typesArr = [{upper},{lower},{symbol},{number}].filter
+    (item => Object.values(item)[0])
+
+
+
+    if(typesCount == 0){
+        return ''
+    }
+
+    for(let i =0; i<length; i+=typesCount){
+        typesArr.forEach(type =>{
+            const funcName = Object.keys(type)[0]
+
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+    const finalPassword = generatedPassword.slice(0, length)
+
+    return finalPassword
+}
+
+//Generator function
+
+function getRandomLower(){
+    return String.fromCharCode(Math.floor(Math.random()*26)+97)
+}
+
+function getRandomUpper(){
+    return String.fromCharCode(Math.floor(Math.random()*26)+65)
+}
+function getRandomNumber(){
+    return String.fromCharCode(Math.floor(Math.random()*10)+48)
+}
+function getRandomSymbol(){
+    const symbols = ["#", "!", "@", "%","*", "&", "+", "=", "-","'", '"','№','_']
+    let res = Math.floor(Math.random()*symbols.length)
+    return symbols[res]
+}
